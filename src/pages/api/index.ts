acab47e571d2354f35390b45f752866d43a75994
ghufron akbar <alan.040219.gam@gmail.com>
2025-04-26 07:20:53 +0700

    Add debugging

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -4,11 +4,16 @@ import { NextApiResponse } from "next";
 import { NextApiRequest } from "next";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.query.db) {
-    const testDb = await db.user.count();
-    return res.status(200).json({ message: "Hello World", testDb });
+  try {
+    if (req.query.db) {
+      const testDb = await db.user.count();
+      return res.status(200).json({ message: "Hello World", testDb });
+    }
+    return res.status(200).json({ message: "Hello World" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal Server Error", error });
   }
-  return res.status(200).json({ message: "Hello World" });
 };
 
 export default handler;
