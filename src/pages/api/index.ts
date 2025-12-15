4fb0d2701981e31e144fd9b4791839f8193c0a06
lanstheprodigy <alan.040219.gam@gmail.com>
2025-12-15 17:08:23 +0700

    feat: Introduce database seeding API endpoint and update Next.js version.

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -5,11 +5,11 @@ import { NextApiRequest } from "next";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    if (req.query.db) {
-      const testDb = await db.user.count();
-      return res.status(200).json({ message: "Hello World", testDb });
-    }
-    return res.status(200).json({ message: "Hello World" });
+
+    const testDb = await db.user.count();
+    return res.status(200).json({ message: "Hello World", testDb });
+
+
   } catch (error) {
     console.log(error);
     return res.status(500).json({ message: "Internal Server Error", error });
